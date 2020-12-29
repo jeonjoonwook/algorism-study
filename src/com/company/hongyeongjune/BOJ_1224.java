378e70ed7f3be3a96938d13e9b94d1a93e7f225f
hongyeongjune <hongyeongjune@naver.com>
2020-12-29 10:25:58 +0900

    solved programmers_보행자천국

--- src/com/company/hongyeongjune/BOJ_1224.java
+++
@@ -1,7 +0,0 @@
-package com.company.hongyeongjune;
-
-public class BOJ_1224 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/com/company/hongyeongjune/firstweek/Programmers_보행자천국.java
@@ -0,0 +1,38 @@
+package com.company.hongyeongjune.firstweek;
+
+public class Solution {
+
+    private static int MOD = 20170805;
+
+    public static int solution(int m, int n, int[][] cityMap) {
+        int answer = 0;
+
+        int[][] right = new int[m+1][n+1];
+        int[][] down = new int[m+1][n+1];
+
+        right[1][1] = 1;
+        down[1][1] = 1;
+
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+
+                if(cityMap[i-1][j-1] == 0) {
+                    right[i][j] += (right[i][j-1] + down[i-1][j]) % MOD;
+                    down[i][j] += (right[i][j-1] + down[i-1][j]) % MOD;
+                }
+                else if(cityMap[i-1][j-1] == 1) {
+                    right[i][j] = 0;
+                    down[i][j] = 0;
+                }
+                else if(cityMap[i-1][j-1] == 2) {
+                    right[i][j] = right[i][j-1];
+                    down[i][j] = down[i-1][j];
+                }
+
+            }
+        }
+
+        return down[m][n];
+    }
+
+}
