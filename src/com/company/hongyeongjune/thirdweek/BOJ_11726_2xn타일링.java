4a411aa95a5d9b87cd2d88567c41230b4f6448b4
hongyeongjune <hongyeongjune@naver.com>
2021-01-12 20:41:17 +0900

    solved BOJ_11726_2 x n 타일링

---
+++ src/com/company/hongyeongjune/thirdweek/BOJ_11726_2xn타일링.java
@@ -0,0 +1,27 @@
+package com.company.hongyeongjune.thirdweek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static int[] dp = new int[1001];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(recursion(n));
+
+    }
+
+    public static int recursion(int n) {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        if(dp[n] != 0) return dp[n];
+        return dp[n] = (recursion(n-1) + recursion(n-2)) % 10007;
+    }
+}
+
