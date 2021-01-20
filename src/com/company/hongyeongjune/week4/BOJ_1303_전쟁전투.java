821feac3d23385f04440b2e8abde9d7da02d8218
hongyeongjune <hongyeongjune@naver.com>
2021-01-20 16:30:36 +0900

    problems description

--- src/com/company/hongyeongjune/week4/BOJ_1303_전쟁전투.java
+++ src/com/company/hongyeongjune/week4/BOJ_1303_전쟁전투.java
@@ -7,6 +7,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    // 가로, 세로 이동을 쉽게하기 위한 배열
     public static int[] dx = {-1, 1, 0, 0};
     public static int[] dy = {0, 0, -1, 1};
     public static int N;
@@ -24,6 +25,7 @@ public class Main {
         war = new char[M][N];
         boolean[][] visited = new boolean[M][N];
 
+        // W, B 담기
         for (int i = 0; i < M; i++) {
             String[] soldier = br.readLine().split("");
             for (int j = 0; j < N; j++) {
@@ -33,13 +35,17 @@ public class Main {
             }
         }
 
+        // W 와 B의 병사의 위력
         int W = 0;
         int B = 0;
 
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
+                // 방문하지 않았다면 DFS 실행
                 if (!visited[i][j]) {
+                    // 서로 인접한 병사들의 수
                     int answer = dfs(i, j, visited, war[i][j]);
+                    // W 병사 B 병사 따로 계산
                     if (war[i][j] == 'W') W += (answer * answer);
                     else B += (answer * answer);
                 }
@@ -55,8 +61,15 @@ public class Main {
         visited[x][y] = true;
 
         for (int i = 0; i < 4; i++) {
+            //dx[i] , dy[i] 의 값
+            // {-1 , 0}  -> 왼쪽으로 한칸
+            // {1 , 0} -> 오른쪽으로 한칸
+            // {0 , -1} -> 아래쪽으로 한칸
+            // {0 , 1} -> 위쪽으로 한칸
             int newX = x + dx[i];
             int newY = y + dy[i];
+            // 범위안에 있고
+            // 같은 병사이고, 방문하지 않았다면 개수 늘리기
             if (newX >= 0 && newX < M && newY >= 0 && newY < N
                     && war[newX][newY] == soldier && !visited[newX][newY]) {
                 count += dfs(newX, newY, visited, soldier);
--- src/com/company/hongyeongjune/week4/BOJ_2606_바이러스.java
+++ src/com/company/hongyeongjune/week4/BOJ_2606_바이러스.java
@@ -16,13 +16,16 @@ public class Main {
         int computer = Integer.parseInt(br.readLine());
         int n = Integer.parseInt(br.readLine());
 
+        // 컴퓨터의 연관을 보여주는 배열
         int[][] node = new int[computer + 1][computer + 1];
+        // 방문 목록
         boolean[] visited = new boolean[computer + 1];
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
+            // 양방향으로 연결
             node[x][y] = node[y][x] = 1;
         }
 
@@ -35,6 +38,7 @@ public class Main {
         visited[start] = true;
 
         for (int i = 0; i < node.length; i++) {
+            // 방문하지 않았고, 서로 연결되어 있다면 answer 의 값을 하나 늘린다.
             if (node[start][i] == 1 && !visited[i]) {
                 dfs(i, node, visited);
                 answer++;
