2d13b361059d122edad3d7f085fb1a21dcd6a1c7
joonwookjeon <46626358+joonwookjeon@users.noreply.github.com>
2021-04-04 17:00:50 +0900

    14주차 주석

--- src/com/company/jeonjoonwook/14주차/CombinationSum2.java
+++ src/com/company/jeonjoonwook/14주차/CombinationSum2.java
@@ -16,8 +16,8 @@ public class CombinationSum2 {
 	public static List<List<Integer>> solve(int[] candidates, int target)
 	{
 		List<List<Integer>> result = new ArrayList<>();
-		Arrays.sort(candidates);
 		
+		Arrays.sort(candidates);		
 		backtracking(result, new ArrayList<>(),candidates,0,target,0);
 		
 		return result;		
@@ -34,12 +34,13 @@ public class CombinationSum2 {
 		}else {
 			for(int i=start; i<candidates.length; i++)
 			{
-				
+				//i가 시작점이 아니고 i-1 값과 같으면 넘긴다. 중복되는 결과가 나오기 때문
 				 if(i !=start && candidates[i] == candidates[i-1]) { 
 					 continue; 
 				 }
 				 			
 				now.add(candidates[i]);
+				//combinationSum 문제와 차이점은 하나의 원소를 여러번 사용 못하므로 i+1 해줌
 				backtracking(result, now, candidates, candidates[i]+sum,target,i+1);
 				now.remove(now.size()-1);
 			}
